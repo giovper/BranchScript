38a85dcc2c23496847a0be16fb3e03ecb6adde45
giovper <gvnn.peruzzi@gmail.com>
2024-09-06 21:18:43 +0200

    Added Pointers Usage

--- main/src/Terminal/Terminal.cpp
+++ main/src/Terminal/Terminal.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 #include <string>
 #include <cstdlib>
-#include "../Utilities/Utilities.h"  // Include il percorso corretto
-#include "../Runtime/Runtime.h"      // Include il percorso corretto
+#include "../Utilities/Utilities.h"
+#include "../Runtime/Runtime.h"
 #include "Terminal.h"
 
 using namespace std;
@@ -11,20 +11,20 @@ string Version = "0.0.1";
 string input;
 
 bool Terminal::ReplyToCLI(const string& input) {
-    Runtime runtime;
+    std::unique_ptr<Runtime> runtime = std::make_unique<Runtime>();
     string main_input = Utilities::divideBySpacing(input, 0);
     if (main_input == "exit") {
         exit(1);
     } else if (main_input == "run" || main_input == "ast") {
         std::string fullPath;
         std::string relPath = Utilities::divideBySpacing(input, 1);
-        fullPath = (((relPath[1] == ':') && (relPath[2] == '\\')) ? relPath : (runtime.startingPath + "\\" + relPath));
+        fullPath = (((relPath[1] == ':') && (relPath[2] == '\\')) ? relPath : (runtime->startingPath + "\\" + relPath));
         cout << "Running File: " << fullPath << std::endl;
         if (Utilities::isFileExisting(fullPath)){
             if (main_input == "run"){
-                runtime.run(Utilities::readFile(fullPath));
+                runtime->run(Utilities::readFile(fullPath));
             } else if (main_input == "ast"){
-                runtime.ast(Utilities::readFile(fullPath));
+                runtime->ast(Utilities::readFile(fullPath));
             }
         } else {
             cout << "File not found :("<<std::endl;
@@ -33,9 +33,8 @@ bool Terminal::ReplyToCLI(const string& input) {
             } else if (fullPath == relPath) {
                 cout << "Check full path";
             } else {
-                cout << "Check relative path. Relative starting path: " << runtime.startingPath;
+                cout << "Check relative path. Relative starting path: " << runtime->startingPath;
             }
-            
         }
     } else if (main_input == "help") {
         cout << "Help page" << endl
@@ -44,7 +43,7 @@ bool Terminal::ReplyToCLI(const string& input) {
         cout << " - help -> Help page";
         cout << " - run *file path* -> Run File";
     } else {
-    	cout<< main_input << " is not recognized as command. Please type help or visit BranchScript documentation";
+        cout << main_input << " is not recognized as command. Please type help or visit BranchScript documentation";
     }
     cout << endl;
     return true;
@@ -53,8 +52,8 @@ bool Terminal::ReplyToCLI(const string& input) {
 void Terminal::ListenToCLI() {
     cout << "BranchScript v" << Version << endl;
     while(true){
-    	cout << ">";
-    	getline(cin, input);
-    	ReplyToCLI(input);
+        cout << ">";
+        getline(cin, input);
+        ReplyToCLI(input);
     }
 }
